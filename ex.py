d10785b63e54e856ca7d97b9605571e20821d59e
shinji-Yama77 <syamas@uw.edu>
2024-09-21 14:03:02 -0700

    plan

--- ex.py
+++ ex.py
@@ -1,1 +1,42 @@
-print("hello word")
+# we have a parser, and read in a few things 
+
+
+
+
+
+# object: 
+
+
+
+
+# create user prompt
+# crafting a user prompt 
+# I {args.age} am cooking } = user prompt
+
+
+# user prompt + query matched with our vectorbase 
+
+# choose document 
+
+# create api request and get respose
+
+# document, user prompt, system prompt
+
+
+# using the documents and the context and ingredient provided, please creates a recipe that uses thee frefences 
+
+
+# response 
+
+# pass response back to user in thi spci 
+
+# pass user prompt,
+
+
+
+
+
+# 
+
+
+
